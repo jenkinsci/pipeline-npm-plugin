c5cd722fa9f0521deb02060ee97f6b01a2cee944
Yash Vardhan <144935197+Ya-shh@users.noreply.github.com>
2024-03-17 18:58:22 +0530

    Delete target directory

--- target/classes/com/github/timothydowney/plugins/pipeline/npm/WithNPMStep/config.groovy
+++
@@ -1,6 +0,0 @@
-package com.github.timothydowney.plugins.pipeline.npm.WithNPMStep
-def f = namespace(lib.FormTagLib) as lib.FormTagLib
-
-f.entry(field: 'npmrcConfig', title: _('npmrc Config')) {
-    f.select()
-}
